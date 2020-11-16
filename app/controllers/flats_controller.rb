c7528a5ffaee62902608b03d4cda9f3b0929cb27
Sheila Leveille <sbleveille@gmail.com>
2020-11-16 01:55:10 -0500

    FEAT: additional comments for students

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,12 +2,14 @@ require "open-uri"
 
 class FlatsController < ApplicationController
   URL = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+
+  # runs our private method set_flats before the index & show actions are run
   before_action :set_flats
 
   def index; end
 
   def show
-    # find the specific flat using the id
+    # finds the specific flat using the id from the params
     @flat = @flats.find { |flat| flat['id'] == params[:id].to_i }
   end
 
